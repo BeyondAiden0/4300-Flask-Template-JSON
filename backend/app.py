93018c0095bc33e6161955fd6c7d06f5f3b42ac7
GraceLiuTQ <102388058+GraceLiuTQ@users.noreply.github.com>
2024-04-27 16:30:22 +0000

    specifications for frontend

--- backend/app.py
+++ backend/app.py
@@ -29,15 +29,35 @@ dish_latentflavors = np.load(dish_latentflavors_path)
 
 @app.route("/")
 def home():
+    """
+    Render the homepage template.
+
+    Returns:
+        render_template: Renders the 'base.html' template for the homepage.
+    """
     return render_template('base.html')
 
 @app.route("/recipe_names", methods=["GET"])
 def recipe_names():
+    """
+    Retrieve all dish names from the application's data source and send them as a JSON response.
+
+    Returns:
+        jsonify: JSON response containing a list of all dish names.
+    """
     # Return all dish names as a list
     return jsonify(name_ing_data[0])
 
 @app.route('/filter_names', methods=['GET'])
 def filter_names():
+    """
+    Process a user's search query to filter and retrieve dish names that closely match the input, using cosine similarity.
+
+    Returns:
+        jsonify: JSON response containing a list of filtered dish names based on the user's query. If an error occurs,
+                 returns a JSON object with an 'error' key and message detailing the exception.
+        status (int): HTTP status code indicating success (200) or internal server error (500).
+    """
     user_input = request.args.get('query', '')  # Get the user input from the query parameters
     if user_input:
         try:
@@ -51,6 +71,12 @@ def filter_names():
 
 @app.route("/store_user_input", methods=["POST"])
 def store_user_input():
+    """
+    Store a user's selected recipe name in a text file on the server.
+
+    Returns:
+        jsonify: JSON response indicating the status and message of the operation; typically confirming successful storage.
+    """
     # Store the selected recipe name in 'input_vector.txt'
     user_input = request.json.get("userInput")
     input_file_path = os.path.join(base_dir, "input_vector.txt")
@@ -60,6 +86,16 @@ def store_user_input():
 
 @app.route("/get_similar_dishes", methods=["POST"])
 def get_similar_dishes():
+    """
+    Retrieve similar dishes based on a user's previously stored input. This function reads the user's input from a file,
+    and then utilizes a series of computations involving flavor profiles and ratings to determine similar dishes.
+
+    Returns:
+        jsonify: JSON response containing a list of dishes similar to the user's input. Each dish includes details such as
+                 name, similarity score, and user ratings. If an error occurs, returns a JSON object with an 'error' key
+                 and message detailing the exception.
+        status (int): HTTP status code indicating success (200) or internal server error (500).
+    """
     # Load the user input from 'input_vector.txt'
     input_file_path = os.path.join(base_dir, "input_vector.txt")
     with open(input_file_path, 'r') as file:
